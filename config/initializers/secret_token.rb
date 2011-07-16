02b9f95f58391b1b1c87404aa42e0c6857530549
Greg Vaughn <gvaughn@gmail.com>
2011-07-16 13:46:33 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eatatjoes::Application.config.secret_token = '0bcd547be0ac9e0e1ad53722e4b9302ebfb1fef0c2bc2e9935aed1faa743a3d533de2823ab3b13a03ebfe31bc59311ca3b05b034a83cd55b0fa2955f2a9d0bf5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Eatatjoes::Application.config.session_store :cookie_store, key: '_eatatjoes_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Eatatjoes::Application.config.session_store :active_record_store
