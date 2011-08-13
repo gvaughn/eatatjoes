bc8913fc5568399a3352f9ddb3385e2e113eff38
Greg Vaughn <gvaughn@gmail.com>
2011-08-13 18:57:43 -0500

    handle order listing

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,4 +16,14 @@ class OrdersController < ApplicationController
     end
   end
 
+  def index
+    @my_orders = current_user.orders
+    @my_retaurants_orders = Order.where :restaurant_id => current_user.restaurant_ids
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @my_retaurants_orders }
+    end
+    
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ActiveRecord::Base
   has_many :order_items, :dependent => :destroy
   belongs_to :restaurant
+  belongs_to :user
   
   def order_items_attributes=(parms_hash)
     parms_hash.each {|k,v| order_items.build v unless v[:quantity].to_i == 0 }
